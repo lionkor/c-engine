2078bfd7c55f29762b0d83c1ff0efdfe7cad243d
Lion Kortlepel <development@kortlepel.com>
2020-06-26 20:16:03 +0200

    add documentation to utils_vector

--- utils/utils_vector.h
+++ utils/utils_vector.h
@@ -11,10 +11,11 @@ typedef struct {
     size_t capacity;
 } Vector;
 
-extern Vector* vector_create(size_t elem_size, size_t capacity);
-extern void    vector_reserve(Vector*, size_t new_size);
-extern void*   vector_append(Vector*, void* element);
-extern void*   vector_at(Vector*, size_t index);
+
+extern Vector* vector_create(size_t elem_size, size_t capacity); // elem_size may not be 0, capacity may be 0.
+extern void    vector_reserve(Vector*, size_t new_size);         // ensures that the vector has the specified size allocated.
+extern void*   vector_append(Vector*, void* element);            // resizes the array and mem-copies the value of *element with assumed size vector->elem_size.
+extern void*   vector_at(Vector*, size_t index);                 // returns a pointer to that index in the array, or NULL on OOB.
 extern void    vector_destroy(Vector*);
 
 #endif // UTILS_VECTOR_H
